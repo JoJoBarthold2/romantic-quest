fca5b40bf85d8d16e74ba99c2315982802d78b86
Johannes Barthold <valentin.j.schmidt@web.de>
2025-03-14 12:33:32 +0100

    basic working api call

--- romantic-quest_main.py
+++ romantic-quest_main.py
@@ -1,1 +1,13 @@
 # romantic-quest_main
+
+import anthropic
+
+client = anthropic.Anthropic(
+    # defaults to os.environ.get("ANTHROPIC_API_KEY")
+)
+message = client.messages.create(
+    model="claude-3-7-sonnet-20250219",
+    max_tokens=1024,
+    messages=[{"role": "user", "content": "I would like a sonnet about love"}],
+)
+print(message.content)
